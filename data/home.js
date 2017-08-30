a0f680db6478fcec4687118bb9abb918f431132a
otfngo <otfngo@qq.com>
2017-08-30 13:37:29 +0800

    初始化

---
+++ data/home.js
@@ -0,0 +1,9 @@
+const slideList = [
+  { src: '/images/home/banner@homepage.png' },
+  { src: '/images/home/banner@homepage.png' },
+  { src: '/images/home/banner@homepage.png' }
+]
+
+module.exports = {
+  slideList: slideList
+}
